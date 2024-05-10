d2ad4e5a2ac938b6b6966046f7ad286283ec0b22
joric <joric@users.noreply.github.com>
2024-05-11 04:53:55 +0500

    added pyUE4Parse support up to 90e309b

--- scripts/supraland_parser.py
+++ scripts/supraland_parser.py
@@ -127,7 +127,8 @@ def export_markers(game, cache_dir, marker_types=marker_types, marker_names=[]):
         outer = {}
         for o in j:
             if 'Outer' in o:
-                outer[':'.join((o['Name'],o['Type'],o['Outer']))] = o
+                outer[':'.join((o['Name'],o['Type'],o['Outer']))] = o # 5e0e6f0
+                outer[':'.join((o['Name'],o['Outer']))] = o # 90e309b
 
         for i, o in enumerate(j):
             prop = o.get('Properties',{})
