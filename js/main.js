5ded62c3db3f4c210614f6cfa8a1aadfa3cb13ad
joric <joric@users.noreply.github.com>
2024-05-15 11:00:45 +0500

    updated files

--- js/main.js
+++ js/main.js
@@ -258,7 +258,7 @@ function loadMap() {
   }
 
   actions = []
-  actions.push(newAction({icon:'&#x1F4C1;', tooltip:'Upload Save File', actions:{'Instructions':'copy-path', 'Load File':'upload-save', 'Unmark All': 'unmark-items' }}));
+  actions.push(newAction({icon:'&#x1F4C1;', tooltip:'Upload Save File', actions:{'Instructions':'copy-path', 'Load Game':'upload-save', 'Unmark All': 'unmark-items' }}));
   let toolbar = new L.Toolbar2.Control({actions: actions, position: 'bottomleft'}).addTo(map);
 
   document.querySelector('.copy-path').onclick = function(e) {
